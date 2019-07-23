e59288206d96c81281bb79b7ef4f15fdfa2b43bd
Redrock912 <leset912@gmail.com>
2019-07-23 16:56:23 +0900

    single components done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,39 +2,66 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 1,
+    tags: []
   };
 
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold"
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+  // constructor() {
+  //   super();
+  //   console.log("Constructor", this);
+  // }
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no tags!</p>;
+    } else
+      return (
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
+      );
+  }
+
+  // arrow function enables this to be called!
+  handleIncrement = product => {
+    console.log(product);
+    // react doesn't change when you change the value!
+    // you need to tell react that it changed!
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  // wrapper for a function is redundant
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-    return classes;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 
-  formatCount() {
-    // picks the 'count' property of 'this.state' :p
-    const { count } = this.state;
-
-    return count === 0 ? "Zero" : count;
+  getBadgeClasses() {
+    let classes = "btn m-2 ";
+    classes += this.state.count === 0 ? "btn-warning" : "btn-primary";
+    return classes;
   }
 }
 
